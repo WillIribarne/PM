84564947a6fe036ec9e994d401912706135276af
Agustina <agustina.andrenacci@gmail.com>
2023-10-06 16:13:59 -0300

    clase RegistroCompras y Compra

---
+++ src/main/java/ModelosHardcodeados/Compra.java
@@ -0,0 +1,9 @@
+
+package ModelosHardcodeados;
+
+public class Compra {
+    private int id_compra;
+    private int id_registro_compra;
+    private Fecha fecha;
+    private float monto;
+}
---
+++ src/main/java/ModelosHardcodeados/RegistroCompras.java
@@ -0,0 +1,7 @@
+
+package ModelosHardcodeados;
+
+public class RegistroCompras {
+    private int id_registro_compras;
+    private int id_perfil;
+}
